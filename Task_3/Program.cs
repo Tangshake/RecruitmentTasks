6d299f8fed7837cfb2b0472fc316afc59dfcdae6
Tangshake <103139132+Tangshake@users.noreply.github.com>
2022-12-15 22:04:40 +0100

    Added Task_3
    It contains only one task.

---
+++ Task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 
+    Write a method that checks if given number (positive integer) contains digit 3.
+    You cannot:
+    - convert number to other type,
+    - use built-in functions like Contains(), StartsWith(), etc.
+*/
+
+uint numberToCheck = 0124567892;
+var result = Check(numberToCheck);
+Console.WriteLine($"Result: {result}");
+
+bool Check(uint number)
+{
+    while(number > 0)
+    {
+        if(number % 10 == 3 ? true : false)
+            return true;
+        else
+            number /= 10;
+    }
+
+    return false;
+}
