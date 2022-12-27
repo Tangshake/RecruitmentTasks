e6e5b9bae5e0b8a98c46f9c1f3d9729c97a82f72
Tangshake <103139132+Tangshake@users.noreply.github.com>
2022-12-27 03:22:51 +0100

    Adding Task_7 with unit test only

---
+++ Task_7/src/FirstLowerNumber/Alghoritm/LowerNumberAlg.cs
@@ -0,0 +1,10 @@
+
+namespace FirstLowerNumber.Alghoritm;
+
+public class LowerNumberAlg
+{
+    public long FindFirstLowerNumber(long number)
+    {
+        return 0;
+    }
+}
---
+++ Task_7/tests/FirstLowerNumberTests/LowerNumberAlgTest.cs
@@ -0,0 +1,37 @@
+using FirstLowerNumber.Alghoritm;
+
+namespace FirstLowerNumberTests;
+
+public class LowerNumberAlgTest
+{
+    [Theory]
+    [InlineData(503, 350)]
+    [InlineData(1234567908, 1234567890)]
+    public void FindFirstLowerNumber_LongNumber_ReturnsLowerNumber(long number, long expectedNumber)
+    {
+        //Arrange
+        var alghoritm = new LowerNumberAlg();
+
+        //Act
+        var actualNumber = alghoritm.FindFirstLowerNumber(number);
+        
+        //Assert
+        Assert.Equal(expected: expectedNumber, actual: actualNumber);
+    }
+
+    [Theory]
+    [InlineData(0, -1)]
+    [InlineData(123, -1)]
+    [InlineData(1027, -1)]
+    public void FindFirstLowerNumber_LongNumber_ReturnsNotFound(long number, long expectedNumber)
+    {
+        //Arrange
+        var alghoritm = new LowerNumberAlg();
+
+        //Act
+        var actualNumber = alghoritm.FindFirstLowerNumber(number);
+        
+        //Assert
+        Assert.Equal(expected: expectedNumber, actual: actualNumber);
+    }
+}
