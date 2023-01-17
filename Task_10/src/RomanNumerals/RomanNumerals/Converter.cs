6c56dafc169ee20d0134df71d2b923e897aa5fd1
Tangshake <103139132+Tangshake@users.noreply.github.com>
2023-01-17 23:50:54 +0100

    Added few more tests

--- Task_10/src/RomanNumerals/RomanNumerals/Converter.cs
+++ Task_10/src/RomanNumerals/RomanNumerals/Converter.cs
@@ -1,12 +1,22 @@
+using System.Text.RegularExpressions;
+
 public class Converter : IConverter
 {
+    Regex reg = new Regex(@"[^IVXLCDM]");
+
     public string ConvertToRoman(int input)
     {
+        if(input <=0 || input >= 4000)
+            throw new ArgumentException("Input number should be > 0 and < 4000");
+
         return "X";
     }
 
     public int ConvertFromRoman(string input)
     {
+        if(string.IsNullOrEmpty(input) || reg.IsMatch(input))
+            throw new ArgumentException();
+
         return 0;
     }
 }
--- Task_10/tests/RomanNumeralsTests/ConverterTest.cs
+++ Task_10/tests/RomanNumeralsTests/ConverterTest.cs
@@ -16,4 +16,30 @@ public class ConverterTest
         //Assert
         Assert.Equal(expected, actual);
     }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(4000)]
+    public void ConvertToRoman_InvalidIntegerNumber_ShouldThrowAnException(int input)
+    {
+        //Arrange
+        var converter = new Converter();
+
+        //Act + Assert
+        Assert.Throws<ArgumentException>(()=> converter.ConvertToRoman(input));
+        
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("XxX")]
+    public void ConvertFromRoman_InvalidInputString_ShouldThrowAnException(string input)
+    {
+        //Arrange
+        var converter = new Converter();
+
+        //Act + Assert
+        Assert.Throws<ArgumentException>(()=> converter.ConvertFromRoman(input));
+    }
 } 
